68510303b7d546ac32c74e2e2e9f59516c1b18ff
thiyagarajanp98 <60639114+thiyagarajanp98@users.noreply.github.com>
2024-01-26 02:40:30 +0530

    Update server.js
    Code running in node js version 20

--- server.js
+++ server.js
@@ -1,187 +1,157 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-const apiLaunchDataUrl = 'https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=web6dot0';
-
-// GET /
-app.get('/', async (req, res) => {
+const jiosaavnApiBaseUrl = "https://www.jiosaavn.com/api.php";
+const ctx = "web6dot0";
+const apiVersion = "4";
+const makeApiRequest = async (url) => {
   try {
-
-    const url = apiLaunchDataUrl;
-    const response = await axios.get(url,{withCredentials: true,
+    const response = await fetch(url, {
       headers: {
-        cookie: 'L=tamil; gdpr_acceptance=true; DL=english',
-      }});
-
-    // Filter the songs for Tamil language
-    const tamilSongs = response.data;
-
-    res.json(tamilSongs);
+        cookie: "L=tamil; gdpr_acceptance=true; DL=english",
+      },
+    });
+    const data = await response.json();
+    return data;
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    throw error;
   }
-});
-
-// details by ID
-
-// GET /song/:query
-app.get('/details/:id/:type/:p/:n', async (req, res) => {
+}; /* GET / */
+app.get("/", async (req, res) => {
   try {
-
-    const id = req.params.id;
-    const type = req.params.type;
-    const p=req.params.p;
-    const n=req.params.n;
-    const url = `https://www.jiosaavn.com/api.php?__call=webapi.get&token=${id}&type=${type}&p=${p}&n=${n}&includeMetaTags=0&ctx=web6dot0&api_version=4&_format=json&_marker=0`;
-    const response = await axios.get(url);
-
-    // Filter the songs for Tamil language
-    const song = response.data
-
-    res.json(song);
+    const url = `${jiosaavnApiBaseUrl}?__call=webapi.getLaunchData&api_version=${apiVersion}&_format=json&_marker=0&ctx=${ctx}`;
+    const launchData = await makeApiRequest(url);
+    res.json(launchData);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      error: error.message || "An error occurred",
+    });
   }
-});
-
-//Song search by string
-const apiSongUrl = 'https://www.jiosaavn.com/api.php?p={page}&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=50&__call=search.getResults&q=';
-
-// GET /search/songs/:query
-app.get('/search/songs/:query', async (req, res) => {
+}); /* GET /details/:id/:type/:p/:n */
+app.get("/details/:id/:type/:p?/:n?", async (req, res) => {
   try {
-    const query = req.params.query;
+    const { id, type, p, n } = req.params;
+    console.log(id, type, p, n);
+    const url = `${jiosaavnApiBaseUrl}?__call=webapi.get&token=${id}&type=${type}&p=${p ? p : 1}&n=${n ? n : 10}&includeMetaTags=0&ctx=${ctx}&api_version=${apiVersion}&_format=json&_marker=0`;
+    const songDetails = await makeApiRequest(url);
+    res.json(songDetails);
+  } catch (error) {
+    res.status(500).json({
+      error: "An error occurred",
+    });
+  }
+}); /* GET /search/songs/:query */
+app.get("/search/songs/:query", async (req, res) => {
+  try {
+    const { query } = req.params;
     let page = 1;
     let allResults = [];
-
-    const url = apiSongUrl.replace('{page}', page);
-    const response = await axios.get(url + query);
-
-    // Filter the songs for Tamil language
-    const tamilSongs = response.data.results.filter((song) => song.language === 'tamil');
-
-    // Append the current page's Tamil songs to the accumulated results
-    allResults = [...allResults, ...tamilSongs];
-
+    while (true) {
+      const url = `${jiosaavnApiBaseUrl}?p=${page}&_format=json&_marker=0&api_version=${apiVersion}&ctx=wap6dot0&n=50&__call=search.getResults&q=${query}`;
+      const response = await makeApiRequest(url);
+      const tamilSongs = response.results.filter(
+        (song) => song.language === "tamil",
+      );
+      allResults.push(...tamilSongs);
+      if (response.results.length < 50) {
+        break;
+      }
+      page++;
+    }
     res.json(allResults);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      error: error.message || "An error occurred",
+    });
   }
-});
-
-//Song details by ID
-const apiSongIDUrl = 'https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=';
-
-// GET /song/:query
-app.get('/song/:id', async (req, res) => {
+}); /* GET /song/:id*/
+app.get("/song/:id", async (req, res) => {
   try {
-
-    const query = req.params.id;
-    const url = apiSongIDUrl;
-    const response = await axios.get(url + query);
-
-    // Filter the songs for Tamil language
-    const song = response.data;
-
-    res.json(song);
+    const { id } = req.params;
+    const url = `${jiosaavnApiBaseUrl}?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=${id}`;
+    const songDetails = await makeApiRequest(url);
+    res.json(songDetails);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      error: "An error occurred",
+    });
   }
-});
-
-const apiAlbumUrl = 'https://www.jiosaavn.com/api.php?p={page}&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=50&__call=search.getAlbumResults&q=';
-
-// GET /search/albums/:query
-app.get('/search/albums/:query', async (req, res) => {
-    try {
-      const query = req.params.query;
-      let page = 1;
-      let allResults = [];
-  
-      const url = apiAlbumUrl.replace('{page}', page);
-      const response = await axios.get(url + query);
-
-      // Filter the albums for Tamil language
-      const tamilAlbums = response.data.results.filter((album) => album.language === 'tamil');
-
-      // Append the current page's Tamil albums to the accumulated results
-      allResults = [...allResults, ...tamilAlbums];
-
-      res.json(allResults);
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred' });
+}); /* GET /search/albums/:query */
+app.get("/search/albums/:query", async (req, res) => {
+  try {
+    const { query } = req.params;
+    let page = 1;
+    let allResults = [];
+    while (true) {
+      const url = `${jiosaavnApiBaseUrl}?p=${page}&_format=json&_marker=0&api_version=${apiVersion}&ctx=wap6dot0&n=50&__call=search.getAlbumResults&q=${query}`;
+      const response = await makeApiRequest(url);
+      const tamilAlbums = response.results.filter(
+        (album) => album.language === "tamil",
+      );
+      allResults.push(...tamilAlbums);
+      if (response.results.length < 50) {
+        break;
+      }
+      page++;
     }
-  });
-
-  //Album details by ID
-
-// GET /album/:query
-app.get('/album/:id', async (req, res) => {
-  try {
-
-    const id = req.params.id;
-    const url = `https://www.jiosaavn.com/api.php?__call=webapi.get&api_version=4&_format=json&_marker=0&ctx=web6dot0&token=${id}&type=album`;
-    const response = await axios.get(url);
-
-    // Filter the songs for Tamil language
-    const album = response.data;
-
-    res.json(album);
+    res.json(allResults);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      error: "An error occurred",
+    });
   }
-});
-  
-  const apiPlaylistUrl = 'https://www.jiosaavn.com/api.php?p={page}&_format=json&_marker=0&api_version=4&ctx=wap6dot0&n=50&__call=search.getArtistResults&q=';
-
-// GET /search/playlists/:query
-app.get('/search/playlists/:query', async (req, res) => {
-    try {
-      const query = req.params.query;
-      let page = 1;
-      let allResults = [];
-  
-      const url = apiPlaylistUrl.replace('{page}', page);
-      const response = await axios.get(url + query);
-
-      // Filter the playlist for Tamil language
-      const tamilPlaylists = response.data.results.filter((playlist) => playlist.more_info.language === 'tamil');
-
-      // Append the current page's Tamil playlist to the accumulated results
-      allResults = [...allResults, ...tamilPlaylists];
-
-      res.json(allResults);
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred' });
+}); /* GET /album/:id*/
+app.get("/album/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const url = `${jiosaavnApiBaseUrl}?__call=webapi.get&api_version=${apiVersion}&_format=json&_marker=0&ctx=web6dot0&token=${id}&type=album`;
+    const albumDetails = await makeApiRequest(url);
+    res.json(albumDetails);
+  } catch (error) {
+    res.status(500).json({
+      error: "An error occurred",
+    });
+  }
+}); /* GET /search/playlists/:query */
+app.get("/search/playlists/:query", async (req, res) => {
+  try {
+    const { query } = req.params;
+    let page = 1;
+    let allResults = [];
+    while (true) {
+      const url = `${jiosaavnApiBaseUrl}?p=${page}&_format=json&_marker=0&api_version=${apiVersion}&ctx=wap6dot0&n=50&__call=search.getArtistResults&q=${query}`;
+      const response = await makeApiRequest(url);
+      const tamilPlaylists = response.results.filter(
+        (playlist) => playlist.more_info.language === "tamil",
+      );
+      allResults.push(...tamilPlaylists);
+      if (response.results.length < 50) {
+        break;
+      }
+      page++;
     }
-  });
-
-  //Playlist details by ID
-const apiPlaylistIDUrl = 'https://www.jiosaavn.com/api.php?__call=playlist.getDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&listid=';
-
-// GET /playlist/:query
-app.get('/playlist/:id', async (req, res) => {
-  try {
-
-    const query = req.params.id;
-    const url = apiPlaylistIDUrl;
-    const response = await axios.get(url + query);
-
-    // Filter the songs for Tamil language
-    const playlist = response.data;
-
-    res.json(playlist);
+    res.json(allResults);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({
+      error: "An error occurred",
+    });
   }
-});
-  
-
-// Start the server
+}); /* GET /playlist/:id */
+app.get("/playlist/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const url = `${jiosaavnApiBaseUrl}?__call=playlist.getDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&listid=${id}`;
+    const playlistDetails = await makeApiRequest(url);
+    res.json(playlistDetails);
+  } catch (error) {
+    res.status(500).json({
+      error: "An error occurred",
+    });
+  }
+}); // Start the server
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
